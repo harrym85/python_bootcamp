182d7bb71969c2612343f323e8dbc4f98d24cd24
harrym85 <harrymapengo@gmail.com>
2019-10-24 15:12:37 +0200

    python

---
+++ Variables.py
@@ -0,0 +1,23 @@
+# Variables
+# Basics types
+# Operation on numbers
+# Operation on string
+
+
+# Numbers and String
+
+
+number1 = 10
+number2 = 7
+string1 = "hello"
+
+# + - / * % ** //
+
+print(number1 + number2)
+print(number1 - number2)
+print(number1 * number2)
+print(number1 / number2)
+print(number1 % number2)
+print(number1 ** number2)
+print(number1 // number2)
+
